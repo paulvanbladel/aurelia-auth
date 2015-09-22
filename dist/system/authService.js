ab45c96469f358e16939cd8f54e3ec7ced2463f5
paul van bladel <paul.vanbladel@gmail.com>
2015-09-22 17:11:17 +0200

    patch specific owin login method issue

--- dist/system/authService.js
+++ dist/system/authService.js
@@ -76,7 +76,7 @@ System.register(['aurelia-framework', 'aurelia-http-client', './authentication',
 
 						var loginUrl = this.auth.getLoginUrl();
 						var content;
-						if (typeof arguments[0] === 'object') {
+						if (typeof arguments[1] !== 'string') {
 							content = arguments[0];
 						} else {
 							content = { 'email': email, 'password': password };
