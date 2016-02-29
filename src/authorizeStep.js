95747dff68169d362341d31122d1615d69ada153
alex <alex@gralak.org>
2016-02-29 16:43:32 +0100

    Fix for incorrect set to array conversion

--- src/authorizeStep.js
+++ src/authorizeStep.js
@@ -13,22 +13,21 @@ export class AuthorizeStep {
   run(routingContext, next) {
     var loginRoute = this.auth.getLoginRoute();
 
-    //TODO what if user is authenticated but has no valid role?
     if (routingContext.getAllInstructions().some(i => !this.auth.isAuthorised(i.config.auth))) {
-      let auth = new Set();
+      let auth = [];
       for(let i of routingContext.getAllInstructions()) {
         if(authUtils.isArray(i.config.auth)) {
           for(let a of i.config.auth) {
-            auth.add(a);
+            auth.push(a);
           }
         }
       }
-      this.auth.setInitialUrl(window.location.href, ...auth);
+      this.auth.setInitialUrl(window.location.href, auth);
       return next.cancel(new Redirect(loginRoute));
     } else if (
         this.auth.isAuthenticated() && //is logged in?
         this.auth.isAuthorised(this.auth.getRequiredRoles()) && //have authorization to access initial route?
-        routingContext.getAllInstructions().some(i => i.fragment) == loginRoute
+        routingContext.getAllInstructions().some(i => i.fragment === loginRoute)
       ) {
       var loginRedirect = this.auth.getLoginRedirect();
       return next.cancel(new Redirect(loginRedirect));
