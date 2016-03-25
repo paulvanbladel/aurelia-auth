e27f9c28b5e58b2582a487114aa6817755baa85c
paul van bladel <paul.vanbladel@gmail.com>
2016-03-25 10:48:13 +0100

    chore(package): use gulp-conventional-changelog

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -12,16 +12,15 @@ gulp.task('bump-version', function(){
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('changelog', function(callback) {
-  var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
-  return changelog({
-    repository: pkg.repository.url,
-    version: pkg.version,
-    file: paths.doc + '/CHANGELOG.md'
-  }, function(err, log) {
-    fs.writeFileSync(paths.doc + '/CHANGELOG.md', log);
-  });
+gulp.task('changelog', function () {
+  return gulp.src(paths.doc + '/CHANGELOG.md', {
+    buffer: false
+  })
+    .pipe(conventionalChangelog({
+      preset: 'angular'
+    }))
+    .pipe(gulp.dest(paths.doc));
 });
 
 gulp.task('prepare-release', function(callback){
