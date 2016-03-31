b1a7f52514e6f5ee6457e5ace218621bdf640672
paul van bladel <paul.vanbladel@gmail.com>
2016-03-31 12:19:59 +0200

    hotfix(oauth2): import status from auth-utilities

--- src/oAuth2.js
+++ src/oAuth2.js
@@ -1,5 +1,5 @@
 import {inject} from 'aurelia-dependency-injection';
-import {extend, forEach, isFunction, isString, joinUrl, camelCase} from './auth-utilities';
+import {extend, forEach, isFunction, isString, joinUrl, camelCase, status} from './auth-utilities';
 import {Storage} from './storage';
 import {Popup} from './popup';
 import {BaseConfig} from './base-config';
